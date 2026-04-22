a601ee7aceb2f10c17709d92f130624fcc815b7f
AjayPeeris <s4088958@student.rmit.edu.au>
2026-04-22 17:57:23 +1000

    Added userRepository

---
+++ backend/src/main/java/com/shareapp/Repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.shareapp.Repository;
+
+import com.shareapp.Model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findByUsername(String username);
+
+    User findByEmail(String email);
+
+    User findbyId(Long id);
+}
