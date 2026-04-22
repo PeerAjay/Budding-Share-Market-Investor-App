bdcf82d1505c5dad0b4909e94285c5b06bc4c0a7
AjayPeeris <s4088958@student.rmit.edu.au>
2026-04-22 13:25:03 +1000

    Added user Model

--- backend/src/main/java/com/shareapp/Model/User.java
+++ backend/src/main/java/com/shareapp/Model/User.java
@@ -1,5 +1,40 @@
 package com.shareapp.Model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    private String username;
+    private String email;
+    private String password;
+
+    public Long getId() {
+        return id;
+    }
+    public String getUsername() {
+        return username;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public String getPassword() {
+        return password;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
     
 }
