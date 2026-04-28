64cb619636939a54534331cf6f40f483ef51368f
AjayPeeris <s4088958@student.rmit.edu.au>
2026-04-28 21:18:38 +1000

    createed Share model

---
+++ backend/src/main/java/com/shareapp/Model/Share.java
@@ -0,0 +1,56 @@
+package com.shareapp.model;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+import com.shareapp.model.Stock;
+import com.shareapp.model.User;
+
+@Entity
+@Table(name = "shares")
+public class Share {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+    private Stock stock;
+    private int quantity;
+    private LocalDateTime purchaseDate;
+    private Double buyPrice;
+
+    public Long getId() {
+        return id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Stock getStock() {
+        return stock;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }   
+
+    public LocalDateTime getPurchaseDate() {
+        return purchaseDate;
+    }
+
+    public Double getBuyPrice() {
+        return buyPrice;
+    }
+
+}
