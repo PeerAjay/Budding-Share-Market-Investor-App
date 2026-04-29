59eb9c9c3524aa6820061ea4e460bf1f0dfdf268
AjayPeeris <s4088958@student.rmit.edu.au>
2026-04-29 21:22:55 +1000

    Transaction service

--- backend/src/main/java/com/shareapp/Service/TransactionService.java
+++ backend/src/main/java/com/shareapp/Service/TransactionService.java
@@ -10,7 +10,25 @@ import com.shareapp.repository.StocksRepository;
 import com.shareapp.model.User;
 import com.shareapp.repository.UserRepository;
 import com.shareapp.model.Holding;
+import com.shareapp.repository.HoldingRepository;
+import com.shareapp.model.TradingAccount;
+import com.shareapp.repository.TradingAccountRepository;
 
 public class TransactionService {
+    private final StocksRepository stocksRepository;
+    private final UserRepository userRepository;
+    private final HoldingRepository holdingRepository;
+    private final TradingAccountRepository tradingAccountRepository;
 
+    public TransactionService(StocksRepository stocksRepository, UserRepository userRepository, HoldingRepository holdingRepository, TradingAccountRepository tradingAccountRepository) {
+        this.stocksRepository = stocksRepository;
+        this.userRepository = userRepository;
+        this.holdingRepository = holdingRepository;
+        this.tradingAccountRepository = tradingAccountRepository;
+    }
+
+    @Transactional
+    public void buyStock(Long userId, String stockSymbol, int quantity) {
+        //TODO: Implement buy stock logic
+    }
 }
