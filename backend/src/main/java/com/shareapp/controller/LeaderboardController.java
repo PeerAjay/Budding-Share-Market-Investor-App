8c9ca7aeb6c5658ee6faf4b2bda9524672fe40b6
AjayPeeris <s4088958@student.rmit.edu.au>
2026-05-20 16:20:58 +1000

    Leaderboard fix

--- backend/src/main/java/com/shareapp/controller/LeaderboardController.java
+++ backend/src/main/java/com/shareapp/controller/LeaderboardController.java
@@ -1,6 +1,8 @@
 package com.shareapp.controller;
 
 import com.shareapp.DataTransferObjects.LeaderboardResponseDTO;
+import com.shareapp.repository.UserRepository;
+import org.springframework.security.core.Authentication;
 import com.shareapp.service.LeaderboardService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,14 +13,24 @@ import org.springframework.web.bind.annotation.*;
 public class LeaderboardController {
 
     private final LeaderboardService leaderboardService;
+    private final UserRepository userRepository;
 
-    public LeaderboardController(LeaderboardService leaderboardService) {
+    public LeaderboardController(LeaderboardService leaderboardService, UserRepository userRepository) {
         this.leaderboardService = leaderboardService;
+        this.userRepository = userRepository;
     }
 
     @GetMapping
-    public ResponseEntity<LeaderboardResponseDTO> getLeaderboard(@RequestParam Long userId) {
-        return ResponseEntity.ok(leaderboardService.getLeaderboard(userId));
+    public ResponseEntity<LeaderboardResponseDTO> getLeaderboard(Authentication authentication) {
+        try {
+            String username = authentication.getName();
+            Long userId = userRepository.findByUsername(username).getId();
+            return ResponseEntity.ok(leaderboardService.getLeaderboard(userId));
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().build();
+        }
+
+        
     }
 
 }
